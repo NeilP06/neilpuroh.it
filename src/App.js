3ab79dac13816dbc0c6bccc37b7ff1569df8da24
NlytxGames <58278838+NlytxGames@users.noreply.github.com>
2023-07-24 23:04:36 -0500

    Added information.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import photo from './photo.png';
 import { Moon } from 'lucide-react';
 import {useState, useEffect } from 'react';
-import logo from './logo.svg';
 import React from 'react';
+import Theme from './Theme.js';
 
 
 export default function App() {
-  const [ theme, setTheme ] = useState("light"); 
-
-  document.body.classList.add("bg-slate-300", "dark:bg-zinc-950");
+  document.body.classList.add("bg-stone-300", "dark:bg-zinc-950");
   return (
     <div>
-      <button><Moon color="959ea8"/></button>
-      <h2 class="text-white">hi</h2>
+      <div class="mt-32 ml-52 flex items-center">
+          <img class="mr-8 border-4 rounded-full border-black dark:border-white" src={photo} height="140" width="140"></img>
+          <div class="inline-grid gap-2">
+            <p class="text-6xl font-mono font-bold text-black dark:text-white">Neil Purohit</p>
+            <p class="font-mono text-black dark:text-white"><a  class="hover:font-semibold" href="https://linkedin.com/in/neilpurohit06"><u>LinkedIn</u></a> // <a class="hover:font-semibold" href="https://github.com/neilp06"><u>Github</u></a> // <a class="hover:font-semibold" href="mailto:neilpurohit42@gmail.com"><u>Contact</u></a> // <a class="hover:font-semibold" href=""><u>Resumé</u></a></p>
+          </div>
+      </div>
+      <div class="mt-10 ml-52 w-2/3">
+        <p class="text-xl font-mono font-medium text-black dark:text-white">I am a senior at Lake Forest High School who has a passion for data analytics & software development, hoping to make a difference in the real world. I am proficient in MERN, Java, Python, & SQL. Some of my notable roles include <i>Summer Worker Intern</i> at AbbVie, <i>Founder</i> of <a class="hover:font-semibold" href=""><u>ProspectiveProgramming</u></a>, and <i>Co-Founder</i> of <a class="hover:font-semibold" href="https://futurepearstudios.co"><u>FuturePearStudios</u></a>, amongst others.</p>
+      </div>
+      <Theme/>
     </div>
   );
 }
