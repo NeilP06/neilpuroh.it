d6c6c11fe6880814d466c059f64bd2be17d16044
NlytxGames <58278838+NlytxGames@users.noreply.github.com>
2023-07-24 20:11:03 -0500

    Add lucide

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { Moon } from 'lucide-react';
+import {useState, useEffect } from 'react';
 import logo from './logo.svg';
-import React from "react";
+import React from 'react';
 
 
 export default function App() {
+  const [ theme, setTheme ] = useState("light"); 
+
   document.body.classList.add("bg-slate-300", "dark:bg-zinc-950");
   return (
     <div>
+      <button><Moon color="959ea8"/></button>
       <h2 class="text-white">hi</h2>
     </div>
   );
 }
+
+
 
