591e22d161d24a6c68679b1bc3f7be1fb52f12a6
NlytxGames <58278838+NlytxGames@users.noreply.github.com>
2023-07-25 13:20:00 -0500

    Fix

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+//     "build": "react-scripts build",
+
+
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('*', (req, res) => { 
   res.sendFile(path.join(__dirname + '/client/build/index.html')) 
